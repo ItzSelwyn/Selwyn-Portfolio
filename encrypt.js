f0de30adb2b1de3469b0ff5dd9c071e22c26f4bf
Selwyn Jesudas <itzselwyn@gmail.com>
2025-09-01 23:21:28 +0530

    Minified CSS and JS

--- encrypt.js
+++ encrypt.js
@@ -1,15 +1,10 @@
-(function() {
-  const parts = ["m", "o", "c", ".", "s", "a", "d", "u", "s", "e", "j", "n", "y", "w", "l", "e", "s", "@", "t", "c", "a", "t", "n", "o", "c"];
-  const email = parts.reverse().join("");
-  const link = document.getElementById("email-link");
-  link.href = "mailto:" + email;
-  link.textContent = email;
-})();
-
-(function() {
-  const phoneParts = ["1", "0", "7", "3", "4", "1", "2", "4", "3", "9", " ", "1", "9", "+"];  
-  const phone = phoneParts.reverse().join("");
-  const link = document.getElementById("phone-link");
-  link.href = "tel:" + phone.replace(/\s+/g, '');
-  link.textContent = phone;
-})();
+!function(){
+  var e=["m","o","c",".","s","a","d","u","s","e","j","n","y","w","l","e","s","@","t","c","a","t","n","o","c"].reverse().join("");
+  const t=document.getElementById("email-link");
+  t.href="mailto:"+e,t.textContent=e}(),
+  
+function(){
+  const e=["1","0","7","3","4"," ","1","2","4","3","9"," ","1","9","+"].reverse().join(""),
+  t=document.getElementById("phone-link");
+  t.href="tel:"+e.replace(/\s+/g,""),
+  t.textContent=e}();
